100214d9ede445c4e2a8f8a420bcb92e20b08a37
Eva Pace <eba.pachi@gmail.com>
2023-04-17 10:56:29 +0200

    build.sh: add glib to pkg-config

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@
 # Usage: ./build.sh basic-tutorial-3
 
 # $1 is the tutorial name, eg: basic-tutorial-3
-gcc "$1".c -o $1 $(pkg-config --cflags --libs gstreamer-1.0)
+gcc "$1".c -o $1 $(pkg-config --cflags --libs gstreamer-1.0 glib)
